909b9153a4673531de695b5c45989a7de82e38df
JulioTog <julio.tognetti@gmail.com>
2017-08-14 17:28:38 -0300

    new

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -29,7 +29,7 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="#">Brand</a>
+      <a class="navbar-brand" href="#">Tri-Podes</a>
     </div>
     <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
