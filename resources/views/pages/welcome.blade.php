80be513a34c9181357453414537fd9762a8abcc8
JulioTog <julio.tognetti@gmail.com>
2017-08-14 11:57:30 -0300

    first html
    -added bootstrap
    -added main html
    -added template

---
+++ resources/views/pages/welcome.blade.php
@@ -0,0 +1,34 @@
+@extends('main')
+
+@section('content')
+
+    <div class="pimg1">
+      <div class="ptext">
+        <span class="border">Tri-Podes</span>
+      </div>
+    </div>
+    <section class="section section-light">
+      <h2>About Us</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea molestias debitis ipsam aliquid nulla in nesciunt vel nisi nihil similique saepe, nam om
+        nis magnam necessitatibus voluptatum veritatis! Non quae optio fuga provident magni neque similique, qui nihil laborum sapiente minus ex itaque qu
+        idem rerum fugiat blanditiis soluta ut, iure voluptas beatae obcaecati. Minus dicta sint, voluptatibus molestias facilis vitae perferendis eaque
+        olores aliquam excepturi, voluptatum tempora earum! Odio unde architecto eligendi libero, quaerat nemo cupiditate excepturi provident, deleniti offi
+        cia dolor, minus repellendus tempore quae voluptates cum necessitatibus obcaecati. Doloribus a ex quibus
+        dam, quia enim velit molestias amet culpa non. Quaerat!</p>
+    </section>
+    <div class="pimg2">
+
+    </div>
+    <section class="section section-dark">
+      <h2>About Us</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea molestias debitis ipsam aliquid nulla in nesciunt vel nisi nihil similique saepe, nam om
+        nis magnam necessitatibus voluptatum veritatis! Non quae optio fuga provident magni neque similique, qui nihil laborum sapiente minus ex itaque qu
+        idem rerum fugiat blanditiis soluta ut, iure voluptas beatae obcaecati. Minus dicta sint, voluptatibus molestias facilis vitae perferendis eaque
+        olores aliquam excepturi, voluptatum tempora earum! Odio unde architecto eligendi libero, quaerat nemo cupiditate excepturi provident, deleniti offi
+        cia dolor, minus repellendus tempore quae voluptates cum necessitatibus obcaecati. Doloribus a ex quibus
+        dam, quia enim velit molestias amet culpa non. Quaerat!</p>
+    </section>
+    <div class="pimg3">
+
+    </div>
+@endsection
